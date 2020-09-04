5196606c339c361938f6c527cb2b412b38ac1c12
Mohamed Elsaed <ashraf@orcas.io>
2020-09-04 23:13:11 +0200

    add htaccess and handel autoload and applicatons class

---
+++ bootstrap/Application.php
@@ -0,0 +1,26 @@
+<?php
+
+
+/**
+ * Class Application
+ */
+class Application
+{
+
+    /**
+     * Application constructor.
+     */
+    private function __construct()
+    {
+    }
+
+    /**
+     * Run the application
+     *
+     * @return void
+     */
+    public static function run(): void
+    {
+        echo 'Hello';
+    }
+}
--- public/index.php
+++ public/index.php
@@ -1,1 +1,36 @@
 <?php
+error_reporting(-1);
+ini_set('display_errors', 'On');
+/**
+ * Php-Framework
+ * @author Mohamed Ashraf Elsaed m.ashraf.saed@gmail.com
+ */
+
+/**
+ * |------------------------------------------------------
+ * | Register AutoLoader
+ * |------------------------------------------------------
+ * | Load the autoloader that will generate classes that well be used
+ * |
+ */
+require __DIR__ . '/../vendor/autoload.php';
+
+
+/**
+ * |------------------------------------------------------
+ * | Bootstrap The Application
+ * |------------------------------------------------------
+ * | Bootstrap the applications and handle framework actions
+ * |
+ */
+require __DIR__ . '/../bootstrap/Application.php';
+
+
+/**
+ * |------------------------------------------------------
+ * | Run Application
+ * |------------------------------------------------------
+ * | Handel Request and response
+ * |
+ */
+$app = Application::run();
